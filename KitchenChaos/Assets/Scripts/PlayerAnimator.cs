469c47b95f92ffaf1f5d9a7e12b23c591b868863
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2024-06-19 14:16:15 -0700

    Add hover to interact and reset project due to issues

--- KitchenChaos/Assets/Scripts/PlayerAnimator.cs
+++ KitchenChaos/Assets/Scripts/PlayerAnimator.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
-{
+public class PlayerAnimator : MonoBehaviour {
+
+
     private const string IS_WALKING = "IsWalking";
+
 
     [SerializeField] private Player player;
 
+
     private Animator animator;
 
-    private void Awake()
-    {
+
+    private void Awake() {
         animator = GetComponent<Animator>();
     }
 
-    private void Update()
-    {
+    private void Update() {
         animator.SetBool(IS_WALKING, player.IsWalking());
     }
+
 }
