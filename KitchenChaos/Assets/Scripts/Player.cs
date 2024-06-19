cfa585137c7088ae8170fb47d576687daf06a282
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2024-06-19 07:23:36 -0700

    Add animation mechanics as well as transitions between idle and walking states

---
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking;
+    private void Update()
+    {
+        UnityEngine.Vector2 inputVector = new UnityEngine.Vector2(0, 0);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+        inputVector = inputVector.normalized;
+
+        UnityEngine.Vector3 moveDir = new UnityEngine.Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != UnityEngine.Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = UnityEngine.Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
